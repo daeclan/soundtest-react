6ec2531d26c57125914639a4e88e1518d743bf18
Declan Murphy <declxn@gmail.com>
2021-04-20 21:00:21 -0500

    Set app.js up for tomorrow's build.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-import React, { useRef } from "react";
-import { Canvas, useFrame } from "react-three-fiber";
-import { Box } from "@react-three/drei/shapes";
+import React from "react";
+import { Canvas } from "react-three-fiber";
 import "./styles.css";
 
-const Scene = () => {
-  const scene = useRef();
-  useFrame(() => {
-    scene.current.rotation.y += 0.04;
-    scene.current.rotation.x += 0.04;
-    scene.current.rotation.z += 0.04;
-  });
-  return (
-    <group ref={scene}>
-      <Box>
-        <meshLambertMaterial attach="material" color="white" />
-      </Box>
-    </group>
-  );
-};
+
 
 export default function App() {
   return (
