e208869937edbad2c1c9f7b553a04c42128765fe
Declan Murphy <declxn@gmail.com>
2021-04-26 20:02:25 -0500

    Added floor to main trunk.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@ import React from "react";
 import { Canvas } from "react-three-fiber";
 import "./styles.css";
 
-
+const Floor = () => {
+  return (
+    <mesh rotation={[Math.PI / 2, 0, 0]} position={[-2, -2, 0]}>
+      <planeBufferGeometry args={[100, 100, 1]} />
+      <meshStandardMaterial color="white" />
+    </mesh>
+  )
+}
 
 export default function App() {
   return (
