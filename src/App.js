d248cc586c7576fd6d507e41705181c0695cc50f
Declan Murphy <declxn@gmail.com>
2021-04-26 22:22:43 -0500

    Removed scene and cleaned up.

--- src/App.js
+++ src/App.js
@@ -5,22 +5,6 @@ import { Box } from "@react-three/drei/shapes";
 import "./styles.css";
 import { MeshStandardMaterial } from "three";
 
-// const Scene = () => {
-//   const scene = useRef();
-//   useFrame(() => {
-//     scene.current.rotation.y += 0.04;
-//     scene.current.rotation.x += 0.04;
-//     scene.current.rotation.z += 0.04;
-//   });
-//   return (
-//     <group ref={scene}>
-//       <Box>
-//         <meshLambertMaterial attach="material" color="white" />
-//       </Box>
-//     </group>
-//   );
-// };
-
 const Floor = () => {
   return (
     <mesh rotation={[-Math.PI / 2, 0, 0]} position={[-2, -2, 0]}>
