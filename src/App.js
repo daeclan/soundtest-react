29786120d745bba0d59cfdd4e5eb2fba075d9de4
Declan Murphy <declxn@gmail.com>
2021-04-26 22:14:30 -0500

    Populated TV and locked down rotation. Also used the Suspense tag to delay loading until it was all congfigured. Neat.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React, { useRef } from "react";
+import React, { useRef, Suspense } from "react";
 import { Canvas, useFrame } from "react-three-fiber";
-import { OrbitControls } from "drei";
+import { OrbitControls, useGLTF } from "drei";
 import { Box } from "@react-three/drei/shapes";
 import "./styles.css";
+import { MeshStandardMaterial } from "three";
 
-const Scene = () => {
-  const scene = useRef();
-  useFrame(() => {
-    scene.current.rotation.y += 0.04;
-    scene.current.rotation.x += 0.04;
-    scene.current.rotation.z += 0.04;
-  });
-  return (
-    <group ref={scene}>
-      <Box>
-        <meshLambertMaterial attach="material" color="white" />
-      </Box>
-    </group>
-  );
-};
+// const Scene = () => {
+//   const scene = useRef();
+//   useFrame(() => {
+//     scene.current.rotation.y += 0.04;
+//     scene.current.rotation.x += 0.04;
+//     scene.current.rotation.z += 0.04;
+//   });
+//   return (
+//     <group ref={scene}>
+//       <Box>
+//         <meshLambertMaterial attach="material" color="white" />
+//       </Box>
+//     </group>
+//   );
+// };
 
 const Floor = () => {
   return (
@@ -29,12 +30,26 @@ const Floor = () => {
   );
 };
 
+const TV = () => {
+  const { nodes } = useGLTF('tv.gltf')
+  return (
+    <group rotation={[Math.PI / 8, Math.PI * 1.2, 0]}>
+      <mesh geometry={nodes.TV.geometry}>
+        <meshStandardMaterial color="white" />
+      </mesh>
+    </group>
+  )
+}
+
 export default function App() {
   return (
     <Canvas>
-      <OrbitControls />
+      <fog attach="fog" args={["black", 1, 7]} />
+      <OrbitControls maxPolarAngle={Math.PI / 2} minPolarAngle={0} />
       <directionalLight intensity={0.5} />
-      <Scene />
+      <Suspense fallback={null}>
+        <TV />
+      </Suspense>
       <Floor />
     </Canvas>
   );
