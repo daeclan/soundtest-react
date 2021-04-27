37b388806c71977c4cf9bd15bfeb478321bd3073
Declan Murphy <declxn@gmail.com>
2021-04-26 20:54:15 -0500

    Added necessary depeny dependencies, box now appears rotating and we got ourselves a floor.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
-import React from "react";
-import { Canvas } from "react-three-fiber";
+import React, { useRef } from "react";
+import { Canvas, useFrame } from "react-three-fiber";
+import { OrbitControls } from "drei";
+import { Box } from "@react-three/drei/shapes";
 import "./styles.css";
+
+const Scene = () => {
+  const scene = useRef();
+  useFrame(() => {
+    scene.current.rotation.y += 0.04;
+    scene.current.rotation.x += 0.04;
+    scene.current.rotation.z += 0.04;
+  });
+  return (
+    <group ref={scene}>
+      <Box>
+        <meshLambertMaterial attach="material" color="white" />
+      </Box>
+    </group>
+  );
+};
 
 const Floor = () => {
   return (
-    <mesh rotation={[Math.PI / 2, 0, 0]} position={[-2, -2, 0]}>
+    <mesh rotation={[-Math.PI / 2, 0, 0]} position={[-2, -2, 0]}>
       <planeBufferGeometry args={[100, 100, 1]} />
       <meshStandardMaterial color="white" />
     </mesh>
-  )
-}
+  );
+};
 
 export default function App() {
   return (
     <Canvas>
+      <OrbitControls />
       <directionalLight intensity={0.5} />
       <Scene />
+      <Floor />
     </Canvas>
   );
 }
